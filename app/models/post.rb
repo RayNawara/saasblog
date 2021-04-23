3acc07035d1efdb025e9483eefbac6b17dbea8dc
RayNawara <rjnawara@gmail.com>
2021-04-23 14:34:52 -0400

    Restrict user from accessing premium posts if not active subscribers

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
 
   validates :title, :content, presence: true
   
+scope :free, -> { where(:premium => false)}
+# Ex:- scope :active, -> {where(:active => true)}
+
   def to_s
     title
   end
